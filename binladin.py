21f69d947909affa47d62c5ebbbbaf475e97f416
rafaelrocks9 <raheel@gjrinternationalschool.edu.in>
2026-03-24 11:49:07 +0530

    Update binladin.py

--- binladin.py
+++ binladin.py
@@ -1,3 +1,14 @@
 import streamlit as st
 st.title("my first streamlit app")
 st.write("hello first web application using streamlit")
+import pandas as pd
+
+data = {
+            "Name": ["rafael", "viggy", "rachit", "Dhruv"],
+            "Age": [25, 30, 35, 40],
+            "City": ["kodagu", "malyali", "bhopali", "up"],
+            "Salary": [50000, 60000, 75000, 80000]
+        }
+
+df = pd.DataFrame(data)
+print(df)
